5203929524dd2cdb4142191674b1a237a0af5abe
Jiwon Kang <kangjw0306@gmail.com>
2025-06-14 20:07:58 +0900

    Initial Commit

---
+++ CurrencyConverter/CurrencyConverterApp.swift
@@ -0,0 +1,17 @@
+//
+//  CurrencyConverterApp.swift
+//  CurrencyConverter
+//
+//  Created by Jiwon Kang on 6/14/25.
+//
+
+import SwiftUI
+
+@main
+struct CurrencyConverterApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
